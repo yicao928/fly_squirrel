935a2f156755419b9c5c33b545c2d1683c9338d5
Haoqin Liang <haoqinliang@umass.edu>
2019-11-30 00:19:24 -0500

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,15 +5,47 @@ class Login extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            msg:'11111'
-        }
+            msg:'11111',
+            username:'',
+            password:''
+        };
     }
+    userchange=(e)=>{
+        let val = this.refs.username.value;
+        this.setState(
+            {
+                username:val
+            }
+        )
+    }
+    passwordcange=(e)=>{
+        let val = this.refs.password.value;
+        this.setState(
+            {
+                password:parseInt(val)
+            }
+        )
+    }
+    checkuser=()=>{
+        if(this.state.username==='admin' && this.state.password===111){
+            alert("success!");
+        }else{
+            alert("username or password wrong!");
+        }
 
+    }
     render(){
         return(
             <div>
-                <a className='Title'>
-                    Login
+                <a className='Login'>
+                username
+                <input ref="username" onChange={this.userchange}/> 
+                <br/><br/>
+                <a className='password'/>
+                password
+                <input ref="password" onChange={this.passwordcange}/>
+                <br/><br/>  
+                <button onClick={this.checkuser}>login</button>
                 </a>
             </div>
         );
